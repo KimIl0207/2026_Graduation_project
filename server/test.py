3f301964b6def56770045236324bd6ff929af3d1
KimIl0207 <163235003+KimIl0207@users.noreply.github.com>
2026-04-02 23:26:15 +0900

    add new models + structure fix

--- server/test.py
+++ server/test.py
@@ -4,6 +4,7 @@ import io
 import os
 from datetime import datetime
 import shutil
+import time
 
 import torch
 import torch.nn as nn
@@ -11,26 +12,11 @@ from torchvision import models, transforms
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+# CORS 설정
 origins = [
     "http://localhost:3001",
     "http://localhost:3000"
 ]
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CNN_MODEL_PATH = os.path.join(BASE_DIR, "model", "best_efficientnet_b0_v2.pth")
-
-print("BASE_DIR:", BASE_DIR)
-print("MODEL_PATH:", CNN_MODEL_PATH)
-print("MODEL_EXISTS:", os.path.exists(CNN_MODEL_PATH))
-
-# 틀린 데이터 저장 폴더
-CORRECTION_SAVE_DIR = "./corrections"
-REAL_DIR = os.path.join(CORRECTION_SAVE_DIR, "real")
-FAKE_DIR = os.path.join(CORRECTION_SAVE_DIR, "fake")
-
-os.makedirs(REAL_DIR, exist_ok=True)
-os.makedirs(FAKE_DIR, exist_ok=True)
 
 app.add_middleware(
     CORSMiddleware,
@@ -40,21 +26,54 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-cnn_model = models.efficientnet_b0(weights=None)
-cnn_model.classifier[1] = nn.Linear(1280, 1)
-cnn_model.load_state_dict(torch.load(CNN_MODEL_PATH, map_location="cpu"))
-cnn_model.eval()
+# 모델 설정
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sd_model_PATH = os.path.join(BASE_DIR, "model", "best_efficientnet_b0_Diffusion.pth")
+midjourney_model_path = os.path.join(BASE_DIR, "model", "best_efficientnet_b0_Midjourney.pth")
+biggan_model_path = os.path.join(BASE_DIR, "model", "best_efficientnet_b0_BigGAN.pth")
 
+sd_model = models.efficientnet_b0(weights=None)
+sd_model.classifier[1] = nn.Linear(1280, 1)
+sd_model.load_state_dict(torch.load(sd_model_PATH, map_location="cpu"))
+sd_model.eval()
+
+midjourney_model = models.efficientnet_b0(weights=None)
+midjourney_model.classifier[1] = nn.Linear(1280, 1)
+midjourney_model.load_state_dict(torch.load(midjourney_model_path, map_location="cpu"))
+midjourney_model.eval()
+
+biggan_model = models.efficientnet_b0(weights=None)
+biggan_model.classifier[1] = nn.Linear(1280, 1)
+biggan_model.load_state_dict(torch.load(biggan_model_path, map_location="cpu"))
+biggan_model.eval()
+
+# 틀린 데이터 저장 폴더
+CORRECTION_SAVE_DIR = "./corrections"
+REAL_DIR = os.path.join(CORRECTION_SAVE_DIR, "real")
+FAKE_DIR = os.path.join(CORRECTION_SAVE_DIR, "fake")
+
+os.makedirs(REAL_DIR, exist_ok=True)
+os.makedirs(FAKE_DIR, exist_ok=True)
+
+
+# 이미지 리사이징
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
+# 파일 크기 제한
 MAX_FILE_SIZE = 10 * 1024 * 1024
+
+@app.get("/")
+async def root():
+    return {"message": "Server is running"}
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
+    # 시간 측정 시작
+    start_time = time.perf_counter()
     image_bytes = await file.read()
 
     if len(image_bytes) > MAX_FILE_SIZE:
@@ -62,21 +81,46 @@ async def predict(file: UploadFile = File(...)):
     
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
     if image.width > 4096 or image.height > 4096:
-        return {"error": "Image resolution must be at least 4096x4096 pixels."}
+        return {"error": "Image resolution must not exceed 4096x4096 pixels."}
     
     image = image.resize((224, 224))
     input_tensor = transform(image).unsqueeze(0)
 
     with torch.no_grad():
-        output = cnn_model(input_tensor)
-        prob = torch.sigmoid(output).item()
+        sd_output = sd_model(input_tensor)
+        sd_prob = torch.sigmoid(sd_output).item()
+
+        mj_output = midjourney_model(input_tensor)
+        mj_prob = torch.sigmoid(mj_output).item()
+
+        bg_ouput = biggan_model(input_tensor)
+        bg_prob = torch.sigmoid(bg_ouput).item()
+
+    end_time = time.perf_counter()
+
+    prob = max(sd_prob, mj_prob, bg_prob)
+    scores = {
+        "Diffusion": sd_prob,
+        "Midjourney": mj_prob,
+        "BigGAN": bg_prob
+    }
+    generator_model = max(scores, key=scores.get) if prob > 0.5 else "Not an ai"
 
     label = "AI Generated" if prob >= 0.5 else "Real Image"
+    # 시간 측정 종료
+    print(f"소요시간: {end_time - start_time}")
+    print(f"sd:{sd_prob}\nmj:{mj_prob}\nbg:{bg_prob}")
 
     return {
         "filename": file.filename,
         "label": label,
-        "probability": round(prob, 4)
+        "probability": round(prob, 4),
+        "generator_model": generator_model,
+        "probs": {
+            "sd": sd_prob,
+            "mj": mj_prob,
+            "bg": bg_prob
+        }
     }
 
 
